5732129e01ccd9699ccd97257a52f170cb90831b
Paula Marroquin <pmmarroquind@correo.url.edu.gt>
2025-10-14 13:51:52 -0600

    cambio paara utf

--- server/db/database.py
+++ server/db/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import text
 from server.db.session import engine
+import io
 
 def init_db_from_sql():
     with engine.connect() as connection:
-        with open("server/schema.sql", "r") as file:
+        with open("server/schema.sql", "r", encoding="utf-8") as file:
             sql_script = file.read()
             connection.execute(text(sql_script))
             connection.commit()
