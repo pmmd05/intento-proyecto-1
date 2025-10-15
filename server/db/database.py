03448e7d8a3c3e5653bb649df961b5bca1669cb9
Paula Marroquin <pmmarroquind@correo.url.edu.gt>
2025-10-14 18:21:42 -0600

    sin server

--- server/db/database.py
+++
@@ -1,9 +0,0 @@
-import os
-from sqlalchemy import text
-from server.db.session import engine
-
-def init_db_from_sql():
-    with engine.connect() as connection:
-        with open("server/schema.sql", "r") as file:
-            sql_script = file.read()
-            connection.execute(text(sql_script))
